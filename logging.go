9a093f5d1f08653b60cea7e0755acf7a8c4daa94
Amit Ziv-Kenet <azivkenet@augury.com>
2016-06-24 10:36:15 +0300

    first commit

---
+++ logging.go
@@ -0,0 +1,45 @@
+package nsqworker
+
+import (
+	"github.com/Sirupsen/logrus"
+	"io/ioutil"
+	"os"
+)
+
+type logWrapper struct {
+	log *logrus.Logger
+	*logrus.Entry
+}
+
+func newLogWrapper(topic, channel string) *logWrapper {
+	log := logrus.New()
+	log.Out = ioutil.Discard
+
+	entry := log.WithFields(logrus.Fields{
+		"topic":	topic,
+		"channel":	channel,
+	})
+
+	return &logWrapper{log, entry}
+}
+
+func (lw *logWrapper) Output(calldepth int, s string) error {
+	lw.Info(s)
+	return nil
+}
+
+func (nw *NsqWorker) ToggleLogging(enable bool) {
+	if enable {
+		nw.log.log = os.Stderr
+	} else {
+		nw.log.log.Out = ioutil.Discard
+	}
+}
+
+func (nw *NsqWorker) ToggleDebug(enable bool) {
+	if enable {
+		nw.log.log.Level = logrus.DebugLevel
+	} else {
+		nw.log.log.Level = logrus.InfoLevel
+	}
+}
---
+++ nsqworker.go
@@ -0,0 +1,51 @@
+package nsqworker
+
+import (
+	"github.com/nsqio/go-nsq"
+	"sync"
+)
+
+type NsqWorker struct {
+	consumer *nsq.Consumer
+	log	*logWrapper
+
+	consumerLock sync.Mutex
+}
+
+func New(topic, channel string) (*NsqWorker, error) {
+
+	nsqw := NsqWorker{}
+	nsqw.log = newLogWrapper(topic, channel)
+	config := nsq.NewConfig()
+
+	var err error
+	nsqw.consumer, err = nsq.NewConsumer(topic, channel, config)
+	if err != nil {
+		nsqw.log.Error(err)
+		return nil, err
+	}
+
+	nsqw.consumer.SetLogger(nsqw.log, nsq.LogLevelInfo)
+
+	return nsqw, nil
+}
+
+func (nsqw *NsqWorker) Start(nsqlookupd string) error {
+	nsqw.log.Infof("connecting nsqworker to nsqlookupd host [%s]", nsqlookupd)
+	return nsqw.consumer.ConnectToNSQLookupd(nsqlookupd)
+}
+
+func (nsqw *NsqWorker) Stop() {
+	nsqw.consumerLock.Lock()
+	defer nsqw.consumerLock.Unlock()
+
+	nsqw.log.Debug("try to stop nsq consumer")
+	nsqw.consumer.Stop()
+	n := <- nsqw.consumer.StopChan
+
+	if n == 0 {
+		nsqw.log.Debug("nsq consumer is already stopped")
+	} else {
+		nsqw.log.Debug("nsq consumer stopped successfully")
+	}
+}
