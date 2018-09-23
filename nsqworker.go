4f01e0b1831b44245b7d0a7eac042f26b6297203
Amit Ziv-Kenet <amit.zivkenet@gmail.com>
2018-09-24 00:26:45 +0300

    added NewWithConfig for initiailization with config struct

--- nsqworker.go
+++ nsqworker.go
@@ -7,27 +7,30 @@ import (
 
 type NsqWorker struct {
 	consumer *nsq.Consumer
-	topic	string
-	channel	string
-	lookupds  []string
+	topic    string
+	channel  string
+	lookupds []string
 
-	log      logrus.FieldLogger
-	internalLogger	*logWrapper
+	log            logrus.FieldLogger
+	internalLogger *logWrapper
 
-	dispatcher   *dispatcher
-	routers	[]Router
+	dispatcher *dispatcher
+	routers    []Router
 
-	running	bool
+	running bool
 }
 
 func New(topic, channel string, lookupds []string) (*NsqWorker, error) {
+	config := nsq.NewConfig()
+	return NewWithConfig(topic, channel, lookupds, config)
+}
 
-	nw := NsqWorker{topic:topic, channel:channel}
+func NewWithConfig(topic, channel string, lookupds []string, config *nsq.Config) (*NsqWorker, error) {
+
+	nw := NsqWorker{topic: topic, channel: channel}
 
 	nw.internalLogger = newLogWrapper(topic, channel)
 	nw.log = nw.internalLogger
-
-	config := nsq.NewConfig()
 
 	var err error
 	nw.consumer, err = nsq.NewConsumer(topic, channel, config)
@@ -54,14 +57,14 @@ func (nw *NsqWorker) RegisterRouter(router Router) error {
 func (nw *NsqWorker) Start() error {
 	nw.consumer.AddHandler(nw.dispatcher)
 
-	nw. running = true
+	nw.running = true
 	nw.log.Infof("connecting nsqworker to nsqlookupd host [%s]", nw.lookupds)
 
-	return  nw.consumer.ConnectToNSQLookupds(nw.lookupds)
+	return nw.consumer.ConnectToNSQLookupds(nw.lookupds)
 }
 
 // Implement the Closer interface
-func (nw *NsqWorker) Close() error{
+func (nw *NsqWorker) Close() error {
 	nw.stopConsumer()
 	return nil
 }
@@ -70,7 +73,7 @@ func (nw *NsqWorker) stopConsumer() {
 
 	nw.log.Debug("try to stop nsq consumer")
 	nw.consumer.Stop()
-	<- nw.consumer.StopChan
+	<-nw.consumer.StopChan
 
 	nw.log.Info("nsq consumer stopped successfully")
 }
